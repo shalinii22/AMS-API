21e7792ab5f9d9ea7d5b8cd34f168a5a92deb949
shalinii22 <48214030+shalinii22@users.noreply.github.com>
2022-05-09 00:40:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -36,13 +36,21 @@ const listener = app.listen(process.env.PORT || 8080, () => {
 mongoose.connect(
   process.env.MONGODB_URI,
   {
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
     server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
     replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } },
   },
   function (err) {
     if (err) return console.log("Error: ", err);
-    console.log("MongoDB Connection -- Ready state is:",mongoose.connection.readyState);
+    console.log(
+      "MongoDB Connection -- Ready state is:",
+      mongoose.connection.readyState
+    );
   }
 );
+
 
 
